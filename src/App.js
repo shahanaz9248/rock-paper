27e77307eb040f8e01fbc0c1cfc7ae64a208f1e0
shahanaz9248 <shaikshannu825@gmail.com>
2025-01-02 14:04:13 +0530

    last commit

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import './App.css';
 
 function App() {
   const[userChoice,setUserChoice]=useState(<div className="user"><h2>You</h2>
-    <img src="/rock.png" width="130" alt="rock" onClick={()=>{check('rock')}}/>
-    <img src="/paper.png" alt="paper" onClick={()=>{check('paper')}}/>
-    <img src="/scissor.png" alt="scissor" onClick={()=>{check('scissor')}}/></div>);
+    <img src="/rock.png" width="100" alt="rock" onClick={()=>{check('rock')}}/>
+    <img src="/paper.png" width="100" alt="paper" onClick={()=>{check('paper')}}/>
+    <img src="/scissor.png" width="100" alt="scissor" onClick={()=>{check('scissor')}}/></div>);
 
   const[computerChoice,setComputerChoice]=useState(<div className="computer">
     <h2>Computer</h2>
-    <img src="/rock.png" width="130" alt="rock"/>
-    <img src="/paper.png" alt="paper"/>
-    <img src="/scissor.png" alt="scissor"/>
+    <img src="/rock.png" width="100" alt="rock"/>
+    <img src="/paper.png"  width="100" alt="paper"/>
+    <img src="/scissor.png" width="100" alt="scissor"/>
    </div>);
 
    const[res,setRes]=useState('');
@@ -33,44 +33,44 @@ function App() {
     }
 
     if(value==='rock' && com==='rock'){
-         setUserChoice(<div className="user"><h2>You have choosen</h2><img src="/rock.png" width="130" alt="rock"/></div>)
-         setComputerChoice(<div className="computer"><h2>Computer has choose</h2><img src="/rock.png" width="130" alt="rock"/></div>)
+         setUserChoice(<div className="user"><h2>You have choosen</h2><img src="/rock.png" width="100" alt="rock"/></div>)
+         setComputerChoice(<div className="computer"><h2>Computer has choose</h2><img src="/rock.png" width="100" alt="rock"/></div>)
          setRes('It is a Tie')
     }else if(value==='rock' && com==='paper'){
-        setUserChoice(<div className="user"><h2>You have choosen</h2><img src="/rock.png" width="130" alt="rock"/></div>)
-        setComputerChoice(<div className="computer"><h2>Computer has choose</h2><img src="/paper.png" alt="paper"/></div>)
+        setUserChoice(<div className="user"><h2>You have choosen</h2><img src="/rock.png" width="100" alt="rock"/></div>)
+        setComputerChoice(<div className="computer"><h2>Computer has choose</h2><img src="/paper.png" width="100" alt="paper"/></div>)
          setRes('Computer has won')
     }else if(value==='rock' && com==='scissor'){
-         setUserChoice(<div className="user"><h2>You have choosen</h2><img src="/rock.png" width="130" alt="rock"/></div>)
-         setComputerChoice(<div className="computer"><h2>Computer has choosen</h2><img src="/scissor.png" alt="scissor"/></div>)
+         setUserChoice(<div className="user"><h2>You have choosen</h2><img src="/rock.png" width="100" alt="rock"/></div>)
+         setComputerChoice(<div className="computer"><h2>Computer has choosen</h2><img src="/scissor.png" width="100" alt="scissor"/></div>)
          setRes('You have won')
     }
 
     if(value==='paper' && com==='rock'){
-      setUserChoice(<div className="user"><h2>You have choosen</h2><img src="/paper.png" alt="paper"/></div>)
-      setComputerChoice(<div className="computer"><h2>Computer has choosen</h2><img src="/rock.png" width="130" alt="rock"/></div>)
+      setUserChoice(<div className="user"><h2>You have choosen</h2><img src="/paper.png" width="100" alt="paper"/></div>)
+      setComputerChoice(<div className="computer"><h2>Computer has choosen</h2><img src="/rock.png" width="100" alt="rock"/></div>)
       setRes('You have won')
     }else if(value==='paper' && com==='paper'){
-     setUserChoice(<div className="user"><h2>You have choosen</h2><img src="/paper.png" alt="paper"/></div>)
-     setComputerChoice(<div className="computer"><h2>Computer has choosen</h2><img src="/paper.png" alt="paper"/></div>)
+     setUserChoice(<div className="user"><h2>You have choosen</h2><img src="/paper.png" width="100" alt="paper"/></div>)
+     setComputerChoice(<div className="computer"><h2>Computer has choosen</h2><img src="/paper.png" width="100" alt="paper"/></div>)
       setRes('It is a Tie')
     }else if(value==='paper' && com==='scissor'){
-      setUserChoice(<div className="user"><h2>You have choosen</h2><img src="/paper.png" alt="paper"/></div>)
-      setComputerChoice(<div className="computer"><h2>Computer has choosen</h2><img src="/scissor.png" alt="scissor"/></div>)
+      setUserChoice(<div className="user"><h2>You have choosen</h2><img src="/paper.png" width="100" alt="paper"/></div>)
+      setComputerChoice(<div className="computer"><h2>Computer has choosen</h2><img src="/scissor.png" width="100" alt="scissor"/></div>)
       setRes('Computer has won')
     }
 
     if(value==='scissor' && com==='rock'){
-     setUserChoice(<div className="user"><h2>You have choosen</h2><img src="/scissor.png" alt="scissor"/></div>)
-     setComputerChoice(<div className="computer"><h2>Computer has choosen</h2><img src="/rock.png" width="130" alt="rock"/></div>)
+     setUserChoice(<div className="user"><h2>You have choosen</h2><img src="/scissor.png" width="100" alt="scissor"/></div>)
+     setComputerChoice(<div className="computer"><h2>Computer has choosen</h2><img src="/rock.png" width="100" alt="rock"/></div>)
      setRes('Computer has won')
     }else if(value==='scissor' && com==='paper'){
-     setUserChoice(<div className="user"><h2>You have choosen</h2><img src="/scissor.png" alt="scissor"/></div>)
-     setComputerChoice(<div className="computer"><h2>Computer has choosen</h2><img src="/paper.png" alt="paper"/></div>)
+     setUserChoice(<div className="user"><h2>You have choosen</h2><img src="/scissor.png" width="100" alt="scissor"/></div>)
+     setComputerChoice(<div className="computer"><h2>Computer has choosen</h2><img src="/paper.png" width="100" alt="paper"/></div>)
      setRes('You have won')
     }else if(value==='scissor' && com==='scissor'){
-     setUserChoice(<div className="user"><h2>You have choosen</h2><img src="/scissor.png" alt="scissor"/></div>)
-     setComputerChoice(<div className="computer"><h2>Computer has choosen</h2><img src="/scissor.png" alt="scissor"/></div>)
+     setUserChoice(<div className="user"><h2>You have choosen</h2><img src="/scissor.png" width="100" alt="scissor"/></div>)
+     setComputerChoice(<div className="computer"><h2>Computer has choosen</h2><img src="/scissor.png" width="100" alt="scissor"/></div>)
      setRes('It is a Tie')
     }
   }
